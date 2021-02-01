fd83bdf564495bed12e24043bcc29c7c3ff4bd0f
Yernar Mukayev <57396558+Yerni-feeling@users.noreply.github.com>
2021-02-01 08:38:27 +0400

    Update dettermine_size.py

--- dettermine_size.py
+++ dettermine_size.py
@@ -6,40 +6,40 @@ cny = cv2.Canny(gry, 50, 200)
 lns = cv2.ximgproc.createFastLineDetector().detect(cny)
 cpy = img.copy()
 
-(h, w) = cpy.shape[:2]
+(height, width) = cpy.shape[:2]
 
-min_x = w
-min_y = h
-max_x = 0
-max_y = 0
+mx = width
+my = height
+Mx = 0
+My = 0
 
 for ln in lns:
-    x1 = int(ln[0][0])
+    a = int(ln[0][0])
     y1 = int(ln[0][1])
-    x2 = int(ln[0][2])
+    b = int(ln[0][2])
     y2 = int(ln[0][3])
 
-    min_x = min(x1, x2, min_x)
-    min_y = min(y1, y2, min_y)
-    max_x = max(x1, x2, max_x)
-    max_y = max(y1, y2, max_y)
+    mx = min(a, b, mx)
+    my = min(y1, y2, my)
+    Mx = max(a, b, Mx)
+    My = max(y1, y2, My)
 
-    print("Coords: ({}, {})->({}, {})".format(x1, y1, x2, y2))
+    print("Coords: ({}, {})->({}, {})".format(a, y1, b, y2))
 
 
-cv2.line(cpy, pt1=(min_x, min_y), pt2=(max_x, min_y), color=(0, 255, 0), thickness=10)
-cv2.putText(cpy, '({}, {})'.format(min_x, min_y), (min_x - 10, (min_y - 30)), cv2.FONT_HERSHEY_SIMPLEX, 2,
+cv2.line(cpy, pt1=(mx, my), pt2=(Mx, my), color=(0, 255, 0), thickness=10)
+cv2.putText(cpy, '({}, {})'.format(mx, my), (mx - 10, (my - 30)), cv2.FONT_HERSHEY_SIMPLEX, 2,
             (0, 255, 0), 3, cv2.LINE_AA)
-cv2.putText(cpy, '({}, {})'.format(max_x, min_y), (max_x - 340, (min_y - 30)), cv2.FONT_HERSHEY_SIMPLEX, 2,
+cv2.putText(cpy, '({}, {})'.format(Mx, my), (Mx - 340, (my - 30)), cv2.FONT_HERSHEY_SIMPLEX, 2,
             (0, 255, 0), 3, cv2.LINE_AA)
-cv2.putText(cpy, '{} pixel'.format((max_x - min_x)), (int((max_x + min_x)/2), (min_y - 30)), cv2.FONT_HERSHEY_SIMPLEX,
+cv2.putText(cpy, '{} pixel'.format((Mx - mx)), (int((Mx + mx)/2), (my - 30)), cv2.FONT_HERSHEY_SIMPLEX,
             2, (0, 255, 0), 3, cv2.LINE_AA)
-cv2.line(cpy, pt1=(min_x, min_y), pt2=(min_x, max_y), color=(0, 0, 255), thickness=10)
-cv2.putText(cpy, '({}, {})'.format(min_x, min_y), (min_x - 10, (min_y + 50)), cv2.FONT_HERSHEY_SIMPLEX, 2,
+cv2.line(cpy, pt1=(mx, my), pt2=(mx, My), color=(0, 0, 255), thickness=10)
+cv2.putText(cpy, '({}, {})'.format(mx, my), (mx - 10, (my + 50)), cv2.FONT_HERSHEY_SIMPLEX, 2,
             (0, 0, 255), 3, cv2.LINE_AA)
-cv2.putText(cpy, '({}, {})'.format(min_x, max_y), (min_x - 10, (max_y - 10)), cv2.FONT_HERSHEY_SIMPLEX, 2,
+cv2.putText(cpy, '({}, {})'.format(mx, My), (mx - 10, (My - 10)), cv2.FONT_HERSHEY_SIMPLEX, 2,
             (0, 0, 255), 3, cv2.LINE_AA)
-cv2.putText(cpy, '{} pixel'.format(max_y - min_y), (min_x - 150, int((max_y + min_y)/2) + 20), cv2.FONT_HERSHEY_SIMPLEX, 2,
+cv2.putText(cpy, '{} pixel'.format(My - my), (mx - 150, int((My + my)/2) + 20), cv2.FONT_HERSHEY_SIMPLEX, 2,
             (0, 0, 255), 3, cv2.LINE_AA)
 cv2.imshow("cpy", cpy)
 cv2.waitKey(0)
